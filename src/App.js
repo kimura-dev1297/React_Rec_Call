6325b2304ef034d4ebb79bb11a2bfebf52c264c0
Scott Reyell <smreyell@gmail.com>
2019-04-24 20:18:12 -0700

    folder structure is all setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {recipes} from './tempList';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
+// Main state is going to be in App and past down to RecipeList
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+        <RecipeList />
+        <RecipeDetails />
+    </React.Fragment>
   );
 }
 
