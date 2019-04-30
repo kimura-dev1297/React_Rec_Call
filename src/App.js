3ce0a564b3b99f086299627609b42fff589ff180
Scott Reyell <smreyell@gmail.com>
2019-04-30 11:55:07 -0700

    Recipe component is all setup with temp data

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import {recipes} from './tempList';
+import { recipes } from './tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 // Main state is going to be in App and past down to RecipeList
 class App extends Component {
   state = {
-    recipes: [],
+    recipes: recipes, // is a temp API call, API limits to 50 an hour
     url: "https://www.food2fork.com/api/search?key=f42946af5e7d8514c9c65074da2c433d&q=chicken%20breast&page=2"
   };
 
-  async getRecipes(){
-    try {
-      const data = await fetch(this.state.url);
-      const jsonData = await data.json();
-      
-      this.setState({
-        recipes: jsonData.recipes
-      })
-    } catch(error){
-      console.log(error);
-    }
-  }
 
-  componentDidMount(){
-    this.getRecipes();
-  }
+  // Will Not be making actual API calls while in develoopment. Will be using the /tempList instead
+
+  // async getRecipes(){
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+      
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     })
+  //   } catch(error){
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount(){
+  //   this.getRecipes();
+  // }
 
   
   render(){
-    console.log(this.state.recipes);
+    // console.log(this.state.recipes);
     return (
       <React.Fragment>
-          <RecipeList />
+          <RecipeList recipes={this.state.recipes} />
           <RecipeDetails />
       </React.Fragment>
     );
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,11 +4,34 @@ import RecipeSearch from './RecipeSearch';
 
 export default class RecipeList extends Component {
   render() {
+    const {recipes} = this.props;
+
     return (
       <React.Fragment>
-        <h1>Hello from List</h1>
         <RecipeSearch />
-        <Recipe />
+        <div className="container my-5">
+        {/* For Nested Elements in Bootstrap I'll Use Nested Rows */}
+        {/*  Title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">Recipe List</h1>
+            </div>
+          </div>
+          {/* End of Title */}
+          <div className="row">
+            {
+              recipes.map( recipe => {
+                return(
+                  // recipes being passed now down through <Recipe/> as "recipe". <Recipe /> is where we will display each recipe
+                  <Recipe 
+                    key={recipe.recipe_id}
+                    recipe={recipe}
+                  />
+                ) 
+              })
+            }
+          </div>
+        </div>
       </React.Fragment>
     )
   }
